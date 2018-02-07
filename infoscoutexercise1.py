3ac8afc043a59db05a5119be51420b296a7e9937
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2018-02-07 14:18:54 -0800

    update menu func exercise 1 and update count hh func to take None exercise 2

--- infoscoutexercise1.py
+++ infoscoutexercise1.py
@@ -4,28 +4,22 @@ transactions = pd.read_csv("https://s3.amazonaws.com/isc-isc/trips_gdrive.csv")
 
 def retailer_affinity(focus_brand, transactions):
 
-    # transactions = pd.read_csv("https://s3.amazonaws.com/isc-isc/trips_gdrive.csv")
     transactions["Parent Brand"] == focus_brand
 
-    # transactions = transactions.head(15)
     transactions['Item Dollars'] = transactions['Item Dollars'].str.replace('$', '')
     transactions['Item Dollars'] = transactions['Item Dollars'].astype(float)
 
     transactions["Total"] = transactions["Item Dollars"] * transactions["Item Units"]
     transactions_by_brand_and_retailer = transactions.groupby(["Parent Brand", "Retailer"])["Total"].sum()
-    # return transactions_by_brand_and_retailer
+   
     total_sales_per_retailer = transactions.groupby(["Retailer"])["Total"].sum()
-    # return total_sales_per_retailer
+    
     return (transactions_by_brand_and_retailer/total_sales_per_retailer)[focus_brand].idxmax()
 
   
 def menu(transactions):
     while True:
-        # print("\nRockstar")
-        # print("Monster")
-        # print("5 Hour Energy")
-        # print("Red Bull")
-
+        
         unique_brands = transactions["Parent Brand"].unique()
         print "\n"
         print "\n".join(unique_brands)
@@ -50,7 +44,3 @@ menu(transactions)
         
 
 
-
-
-
-# print retailer_affinity(brand, transactions)
--- infoscoutexercise2.py
+++ infoscoutexercise2.py
@@ -2,41 +2,82 @@ import pandas as pd
 from datetime import datetime
 
 
+transactions = pd.read_csv("https://s3.amazonaws.com/isc-isc/trips_gdrive.csv")
 
-def count_hhs(brand, retailer, start_date, end_date):
+def count_hhs(brand, retailer, start_date, end_date, transactions):
 
-    transactions = pd.read_csv("https://s3.amazonaws.com/isc-isc/trips_gdrive.csv")
+    query = 1
+
+
     transactions['Date'] = pd.to_datetime(transactions.Date)
     transactions["Date"]
     
-    start_date = datetime.strptime(start_date, '%Y-%m-%d')
-   
-    end_date = datetime.strptime(end_date, '%Y-%m-%d')
-    
-    
-    
+  
     if(brand == None and retailer == None and start_date == None and end_date == None):
         return transactions
 
-    # # query = 1
 
-    if(brand != None and retailer != None and start_date != None and end_date != None):
-        # filtered_transactions = transactions[1 and (transactions["Parent Brand"] == brand) & (transactions["Retailer"] == retailer) & (transactions["Date"] > start_date) & (transactions["Date"] < end_date)]
-        filtered_transactions = transactions[1 and (transactions["Parent Brand"] == brand) & (transactions["Retailer"] == retailer) & (transactions["Date"] > start_date) & (transactions['Date']< end_date)]
-        unique = filtered_transactions["User ID"].unique()
-        count_hh_costco_monster = len(unique)
-        return count_hh_costco_monster
+    if (brand != None):
+        query = 1 and (transactions["Parent Brand"] == brand)
+    
+    if (retailer != None):
+        query =  query & (transactions["Retailer"] == retailer) 
 
 
-    # if(retailer != None):
-    #     query = transactions[query & (transactions["Retailer"] == retailer)]
+    if (start_date!= None):
+        start_date = datetime.strptime(start_date, '%Y-%m-%d')
+        query = query & (transactions['Date'] > start_date) 
+        
+    
+    if (end_date!= None):
+        end_date = datetime.strptime(end_date, '%Y-%m-%d')
+        query = query & (transactions['Date'] < end_date)
+    
 
-    # return transactions[query]
+    
+    filtered_transactions = transactions[query]
+    unique = filtered_transactions["User ID"].unique()
+    count_hh_= len(unique)
+    return count_hh_
 
-    # filtered_transactions = transactions[1 & (transactions["Parent Brand"] == "Monster") & (transactions["Retailer"] == "Costco")]
+
+print count_hhs("5 Hour Energy","Walmart", None, "2017-12-12", transactions)
+# def menu(transactions):
+#     while True:
+        
+#         unique_brands = transactions["Parent Brand"].unique()
+#         print "\n"
+#         print "\n".join(unique_brands)
+#         print None
+#         print ( "q to quit the program.\n")
+
+#         brand = raw_input("\nPlease pick one of the brands from the menu options: Rockstar, Monster, 5 Hour Energy, Red Bull, or q.\n")
+
+#         unique_retailers = transactions["Retailer"].unique()
+
+#         print "\n"
+#         print "\n".join(unique_retailers)
+#         print None
+#         print ( "q to quit the program.\n")
+
+#         retailer = raw_input("\nPlease pick one of the Retailers from the menu options: Walmart, Walgreens, Kroger, CVS, Publix, Costco, Target, Safeway, None, or q.\n")
 
 
-print count_hhs("Red Bull", "Costco", "2014-6-1", "2017-4-1")
+#         if brand in unique_brands or None and retailer in unique_retailers or None:
+#             print "Your answer is: "
+#             print count_hhs(brand, retailer, "2014-6-1", "2017-4-1", transactions)
+#             print "\n"
+#             break
+#         elif brand == "q":
+#             break       
+#         else :
+#             print "Please re run this program and re input one of the selections again.\n"
+            
+
+
+# menu(transactions)
+
+
 
 
 
